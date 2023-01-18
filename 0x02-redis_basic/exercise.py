4382944ea156cc2f59609fcf544d2c7f217c9f36
Jesica-akeyo <jessica.akeyo@gmail.com>
2023-01-18 21:15:36 +0300

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,32 +1,94 @@
 #!/usr/bin/env python3
-""" Writing strings to Redis  """
-from typing import Callable, Optional, Union
+""" redis module
+"""
 import redis
-import uuid
+from uuid import uuid4
+from typing import Union, Optional, Callable
+from functools import wraps
+
+
+UnionOfTypes = Union[str, bytes, int, float]
+
+
+def count_calls(method: Callable) -> Callable:
+    """count number of calls
+        Callable: [method] """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """wrapper of decorator"""
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """ number of history inputs"""
+    inputs = method.__qualname__ + ":inputs"
+    outputs = method.__qualname__ + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """wrapper of decorator"""
+        self._redis.rpush(inputs, str(args))
+        returned_method = method(self, *args, **kwds)
+        self._redis.rpush(outputs, str(returned_method))
+        return returned_method
+    return wrapper
+
+
+def replay(method: Callable):
+    """ replay to display hstory of calls"""
+    self_ = method.__self__
+    stored_name = method.__qualname__
+    stored_key = self_.get(stored_name)
+    if stored_key:
+        times = self_.get_str(stored_key)
+        inputs = self_._redis.lrange(stored_name + ":inputs", 0, -1)
+        outputs = self_._redis.lrange(stored_name + ":outputs", 0, -1)
+
+        print(f"{stored_name} was called {times} times:")
+        zipvalues = zip(inputs, outputs)
+        result_list = list(zipvalues)
+        for k, v in result_list:
+            name = self_.get_str(k)
+            val = self_.get_str(v)
+            print(f"{stored_name}(*{name}) -> {val}")
 
 
 class Cache:
-    """ Cache class """
+    """ Cache redis class
+    """
+
     def __init__(self):
-        """ constructor """
+        """ constructor for redis model
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ Generates a random key """
-        id = str(uuid.uuid4())
-        self._redis.set(id, data)
-        return id
+    @call_history
+    @count_calls
+    def store(self, data: UnionOfTypes) -> str:
+        """store data into redis cache"""
+        key = str(uuid4())
 
-    def get_str(self, key):
-        """  parametrize Cache.get with the correct conversion function """
-        return self.get(key, str)
+        self._redis.mset({key: data})
+        return key
 
-    def get_int(self, key):
-        """  parametrize Cache.get with the correct conversion function """
-        return self.get(key, int)
+    def get(self, key: str, fn: Optional[Callable] = None)\
+            -> UnionOfTypes:
+        """get key from redis"""
+        if fn:
+            return fn(self._redis.get(key))
+        data = self._redis.get(key)
+        return data
 
-    def get(self, key: str, fn: Optional[Callable] = None):
-        """ convert the data back to the desired format """
-        val = self._redis.get(key)
-        return val if not fn else fn(val)
+    def get_str(self, string: bytes) -> str:
+        """ get a string """
+        return string.decode("utf-8")
+
+    def get_int(self, number: int) -> int:
+        """ get int value"""
+        result = 0 * 256 + int(number)
+        return result
